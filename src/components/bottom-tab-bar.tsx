e0d5393d1db96fe128de0fbbb2c72a57f3c76f7f
hongci9999 <hongci9999@gmail.com>
2026-07-09 22:29:31 +0900

    Revert "perf: 헤더/하단탭 nav 링크 prefetch 비활성화"
    This reverts commit 894e1977a580bda2f382887b02e51fc96bc1a00e.

--- src/components/bottom-tab-bar.tsx
+++ src/components/bottom-tab-bar.tsx
@@ -46,7 +46,6 @@ export default function BottomTabBar() {
           <Link
             key={href}
             href={href}
-            prefetch={false}
             style={{
               display: 'flex',
               flexDirection: 'column',
